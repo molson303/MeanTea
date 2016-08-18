4472d1d777074f2ca7f4f8ae4e216824bb1febe5
MIchael <michael.olson756@gmail.com>
2016-08-18 16:53:52 -0600

    setup complete, pages created

---
+++ app.js
@@ -0,0 +1,13 @@
+var app = angular.module('meanTea', ['ngRoute']);
+app.config (function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/main.html',
+      controller: 'MainController'
+    })
+    .when('/new', {
+      templateUrl: 'templates/caffeineMeter.html',
+      controller: ''
+    })
+    .otherwise('/');
+})
