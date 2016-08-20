30e2f5be6b60b40ea040255327ba21428fa295cb
MIchael <michael.olson756@gmail.com>
2016-08-19 20:41:05 -0600

    unable to loop through info on cart page

--- controllers/checkout_controller.js
+++ controllers/checkout_controller.js
@@ -1,23 +1,10 @@
 app.controller('checkout_controller', function ($scope, teaService) {
 $scope.vw = {}
-$scope.vw.teaName = teaService.inventory[0].name;
-$scope.vw.teaImage = teaService.inventory[0].imageUrl;
-$scope.vw.teas = teaService.inventory;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// $scope.vw.teaName = teaService.inventory[0].name;
+// $scope.vw.teaImage = teaService.inventory[0].imageUrl;
+$scope.inventory = teaService.inventory;
+console.log("*********");
+console.log($scope.inventory.name);
 
 
 
