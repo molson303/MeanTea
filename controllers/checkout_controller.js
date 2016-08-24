f220ec3a4b2220829ef6b11a0961dff745154c93
MIchael <michael.olson756@gmail.com>
2016-08-23 18:22:06 -0600

    checkout page refactored and working

--- controllers/checkout_controller.js
+++ controllers/checkout_controller.js
@@ -1,7 +1,6 @@
 app.controller('checkout_controller', function ($scope, teaService) {
 $scope.vw = {}
-// $scope.vw.teaName = teaService.inventory[0].name;
-// $scope.vw.teaImage = teaService.inventory[0].imageUrl;
+
 $scope.inventory = teaService.inventory;
 console.log("*********");
 console.log($scope.inventory);
